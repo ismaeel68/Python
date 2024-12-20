3d5e3e49ddc84288b5c60c8e20f52477294a37bc
Ismaeel <ismaeelmh75@gmail.com>
2024-12-20 19:52:17 +0100

    primer commit

---
+++ First Video/Variables and date types.py
@@ -0,0 +1,67 @@
+name = "Ismaeel"
+age = 16
+price = 25.99
+
+
+print (type(name))
+print (type(age))
+print (type(price))
+
+age = 16
+Old = False 
+a = None
+
+print (type(Old))
+print (type(a))
+
+a = 2
+b = 5
+sum = a - b
+print(sum)
+
+#artihmetic operators 
+a = 5
+b = 2
+print (a + b)
+print (a % b) #modelo/reminder 
+print (a ** b) #a^b
+#relational operators it give only answer in True o False 
+a = 50
+b = 20
+print (a == b) #False 
+
+a = 50
+b = 20 
+print (a != b) # this is sign is showing for not equal to the value 
+
+# a = 50
+# b = 20
+# print (a >= b) 
+# print (a > b)
+# print (a <= b) 
+# print (a < b) 
+# Assigment Operators 
+num = 10
+num = num + 10 # => 20
+num += 10 # these 2 are the same comands 
+print(num)
+
+#Logical operators that work at the bolean 
+first type of logical operators is not (not) give the value opposite 
+a = 50
+b = 30  
+print(not False)
+print(not (a > b))
+
+# Second type of logical operators is and (and) is work on 2 values 
+#  val1 = True 
+#  val2 = False  
+#  print("and operator:", val1 and val2) 
+#  # Third type of logical operators is OR (Or) is work on 1 true 
+#  print("or operator:", val1 or val2) 
+ 
+ #type Conversion 
+a =(input) 
+b = 4.5
+sum = (a + b)
+print(sum)
---
+++ Second Video/Lecture_two.py
@@ -0,0 +1,25 @@
+str1= "Thora.\nwe creating it in python"
+str2= "Pagal.\t we creating in pyhton"
+str3= "Kaliya"
+print(str1,str2,str3)
+#lenght function
+str1="Thora"
+print(len(str1))
+str2="kaliya"
+print(str1+str2)
+# #Indexing [in indexing starting with the 0 not with the 1]
+str="Thora"
+ch = str[4]
+print(ch)
+# positive Slicing [in silicing has starting_idx:but it has no ending idx]
+str="Thora"
+print(str[0:5])# if i miss te first letter o the last letter python know about the last o the first and he write it himself
+# #negative slicing [in the negative silicing is start from the last number with the -1]
+str="Thora"
+print(str[-3:-1])
+#function if i check my string is end with this letter if this is correcct give me the output True and if it is incorrect give me the output False
+str="I am studing python"
+print(str.endswith("hon"))
+#capitalize In my string if i write my first letter in small and i write this capitalize in the command it shows into the Capital letter its create a new string not modifed the original string
+str="you am studying python"
+print(str.upper())
