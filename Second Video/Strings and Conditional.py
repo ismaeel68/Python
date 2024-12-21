df7c1381a0eb344f3a1c6580dccda1e4c4e5340c
Ismaeel <ismaeelmh75@gmail.com>
2024-12-21 12:29:57 +0100

    Today work

--- Second Video/Strings and Conditional.py
+++ Second Video/Strings and Conditional.py
@@ -78,16 +78,3 @@ num=14
 rem=num % 2
 if(rem==0):
     print("Even")
-else:
-    print(ODD)
-
-
-a=int(input("enter first number: "))
-b=int(input("enter second number: "))
-c=int(input("enter third number: "))
-if(a>=b and a>=c):
-    print("first number is largest", a)
-elif(b >= c):
-    print("second number is largest", b)
-else:
-    print("third is largest", c)
