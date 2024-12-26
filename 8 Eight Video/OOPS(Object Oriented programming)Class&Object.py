585cd1b8907db4c400031f2d359ee950f14678f3
Ismaeel <ismaeelmh75@gmail.com>
2024-12-26 13:38:48 +0100

    morning work

---
+++ 8 Eight Video/OOPS(Object Oriented programming)Class&Object.py
@@ -0,0 +1,18 @@
+#(OOPS in python) To map with real world scenarios,we started using object in code.
+#(Class & Object) in python 
+#Class is a blue print for creating object.
+class Student: #This is a class 
+    name="Karan"
+S1 = Student()#This is a object
+print(S1.name)
+
+s2 = Student()
+print(s2.name)
+
+class Car:
+    color="Black"
+    brand="Mercedes"
+car1 = Car
+print(car1.color)
+print(car1.brand)
+#(Constructor)
